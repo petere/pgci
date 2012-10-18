71f5bc18728d7e22a17f099ad0ad00a84c63d731
Peter Eisentraut <peter@eisentraut.org>
2012-10-18 02:14:05 -0400

    Add postgresql build job helper

---
+++ jobhelpers/postgresql-build.sh
@@ -0,0 +1,6 @@
+./configure --enable-debug --enable-depend --enable-cassert --with-tcl --with-perl --with-python --with-krb5 --with-pam --with-ldap --with-openssl --with-libxml --with-libxslt --with-gssapi --enable-thread-safety --enable-nls --with-ossp-uuid
+make world
+make maintainer-check
+make check-world
+make clean
+make distcheck
