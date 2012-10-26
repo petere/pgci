3101a8edf485e2a6f18b8a3a5219d1c86a5681c0
Peter Eisentraut <peter@eisentraut.org>
2012-10-26 01:49:20 -0400

    Support older PostgreSQL versions in build script

--- jobhelpers/postgresql-build.sh
+++ jobhelpers/postgresql-build.sh
@@ -1,4 +1,19 @@
 ./configure --enable-debug --enable-depend --enable-cassert --with-tcl --with-perl --with-python --with-krb5 --with-pam --with-ldap --with-openssl --with-libxml --with-libxslt --with-gssapi --enable-thread-safety --enable-nls --with-ossp-uuid
-make world
-make maintainer-check
-make check-world
+
+if grep -qw world GNUmakefile; then
+	make -k world
+else
+	make -k all
+	make -k -C contrib all
+	make -k -C doc/src/sgml all
+fi
+
+if grep -qw maintainer-check GNUmakefile; then
+	make -k maintainer-check
+fi
+
+if grep -qw check-world GNUmakefile; then
+	make -k check-world
+else
+	make -k check
+fi
