47a5e5243f9ccc4ef314a0fe5ee27c6f73c97af9
Peter Eisentraut <peter@eisentraut.org>
2012-10-26 20:23:16 -0400

    Move Icinga configuration to module

---
+++ modules/icinga/manifests/init.pp
@@ -0,0 +1,24 @@
+class icinga {
+
+  package {
+    'icinga':
+      ensure => installed,
+  }
+
+  file {
+    ['/etc/icinga', '/etc/icinga/objects']:
+      ensure => directory,
+      require => Package ['icinga'];
+
+    '/etc/icinga/objects/pgci.cfg':
+      ensure => present,
+      content => template('icinga/pgci.cfg.erb'),
+      notify => Service['icinga'];
+  }
+
+  service {
+    'icinga':
+      ensure => running,
+      require => Service['httpd', 'ntp', 'postfix'],
+  }
+}
