58144cb83e315f1b27cf52c3f6fdcfb9e5e0a861
Peter Eisentraut <peter@eisentraut.org>
2012-11-12 19:14:06 -0500

    Configure Java memory for FOP

--- jobhelpers/postgresql-extradoc.sh
+++ jobhelpers/postgresql-extradoc.sh
@@ -1,5 +1,8 @@
 test -e GNUmakefile || ./configure
 cd doc/src/sgml
 make -k xslthtml postgres-A4.pdf postgres-US.pdf postgres.info postgres-A4.fo postgres-US.fo
+
+FOP_OPTS='-Xmx448m'
+export FOP_OPTS
 fop -fo postgres-A4.fo -pdf postgres-A4-fop.pdf
 fop -fo postgres-US.fo -pdf postgres-US-fop.pdf
