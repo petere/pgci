f34b07532afcab89a362266d1c536494c80d9ac5
Peter Eisentraut <peter@eisentraut.org>
2013-03-01 23:04:22 -0500

    Call fop from a makefile rule
    That way, it is not called when nothing needs to be rebuilt, like the
    other targets are handled.

--- jobhelpers/postgresql-extradoc.sh
+++ jobhelpers/postgresql-extradoc.sh
@@ -1,8 +1,13 @@
-test -e GNUmakefile || ./configure
-cd doc/src/sgml
-make -k xslthtml postgres-A4.pdf postgres-US.pdf postgres.info postgres-A4.fo postgres-US.fo epub
+cat <<'EOF' >src/Makefile.custom
+.SECONDARY: postgres-A4.fo postgres-US.fo
+
+%-fop.pdf: %.fo
+	/usr/bin/time -v fop -fo $< -pdf $@
+EOF
 
 JAVA_ARGS='-Xmx700m -Xincgc'
 export JAVA_ARGS
-/usr/bin/time -v fop -fo postgres-A4.fo -pdf postgres-A4-fop.pdf
-/usr/bin/time -v fop -fo postgres-US.fo -pdf postgres-US-fop.pdf
+
+test -e GNUmakefile || ./configure
+cd doc/src/sgml
+make -k xslthtml postgres-A4.pdf postgres-US.pdf postgres.info postgres-A4-fop.pdf postgres-US-fop.pdf epub
