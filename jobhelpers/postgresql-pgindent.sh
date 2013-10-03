04026a90de0a84359c0d03b9b94d3c33419671e5
Peter Eisentraut <peter@eisentraut.org>
2013-10-02 20:51:19 -0400

    Download indent via http instead of ftp

--- jobhelpers/postgresql-pgindent.sh
+++ jobhelpers/postgresql-pgindent.sh
@@ -1,4 +1,4 @@
-wget -N ftp://ftp.postgresql.org/pub/dev/pg_bsd_indent-1.2.tar.gz
+wget -N http://ftp.postgresql.org/pub/dev/pg_bsd_indent-1.2.tar.gz
 echo '34991f8830eb4f8125b423792155a4763dded4a3  pg_bsd_indent-1.2.tar.gz' | sha1sum -c
 tar xvf pg_bsd_indent-1.2.tar.gz
 make -C pg_bsd_indent/
@@ -17,4 +17,4 @@ git diff --src-prefix=original/ --dst-prefix=pgindent/ --patch --stat --dirstat 
 make
 make check
 make -C contrib
-#make -C contrib
+#make -C contrib check
